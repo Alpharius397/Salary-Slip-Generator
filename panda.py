a812cdc9895fdcc9a8ab4b0102cd1c586290b28d
Alpharius397 <155239372+Alpharius397@users.noreply.github.com>
2024-06-20 21:12:59 +0530

    Delete panda.py

--- panda.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import random
-file_path = 'Sample sheet for salary calculation and salary slip (1).xlsx'
-ans=[]
-
-res = pd.read_excel(file_path)
-res.fillna(random.randint(0,10000),axis=1,inplace=True)
-print(res)
