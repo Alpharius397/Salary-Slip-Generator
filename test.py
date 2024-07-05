d8899f45c6143ed7b388365a2ac0cc546824d805
Alpharius397 <155239372+Alpharius397@users.noreply.github.com>
2024-07-05 19:36:21 +0530

    Update test.py

--- test.py
+++ test.py
@@ -12,11 +12,11 @@ class Database():
                 )
     
     # create a table from month and year if it does not exist
-    def createData(self,month:str,year:int,columns:list[str]) -> int:
+    def createData(self,month:str,year:int,columns:list[str],insti:str,type:str) -> int:
         cursor = self.db.cursor()
 
         try:
-            sql = f"CREATE TABLE {month}_{year}({','.join([ col + ' VARCHAR(225) PRIMARY KEY' if col=='HR_EMP_CODE' else col + ' VARCHAR(225)' for col in columns])})"
+            sql = f"CREATE TABLE {insti}_{type}_{month}_{year}({','.join([ col + ' VARCHAR(225) PRIMARY KEY' if col=='HR_EMP_CODE' else col + ' VARCHAR(225)' for col in columns])})"
             cursor.execute(sql)
 
             print('Table Created')
@@ -28,7 +28,7 @@ class Database():
             return 0
 
     # updates existing data or inserts new data
-    def updateData(self,data,month:str,year:int) -> None:
+    def updateData(self,data,month:str,year:int,insti:str,type:str) -> None:
         cursor = self.db.cursor()
 
         for i in data['HR_EMP_CODE']:
@@ -36,10 +36,10 @@ class Database():
             query =','.join([f"{col}='{new[col]}'" for col in new if col!='HR_EMP_CODE'])
             
             try:
-                cursor.execute(f"INSERT INTO {month}_{year} ({','.join(new.keys())}) VALUE ({','.join([f"'{i}'" for i in new.values()])})")
+                cursor.execute(f"INSERT INTO {insti}_{type}_{month}_{year} ({','.join(new.keys())}) VALUE ({','.join([str(i) for i in new.values()])})")
 
             except mysql.connector.errors.ProgrammingError as e:
-                cursor.execute(f'UPDATE {month}_{year} SET {query} WHERE HR_EMP_CODE={i}')
+                cursor.execute(f'UPDATE {insti}_{type}_{month}_{year} SET {query} WHERE HR_EMP_CODE={i}')
             except:
                 print('Not MySQL Error!')
                 return None
@@ -57,7 +57,7 @@ class Database():
                 return False
          
         cursor = self.db.cursor()
-        memo = defaultdict(list)
+        memo = {}
         try:
             cursor.execute('show tables')
         except:
@@ -65,19 +65,24 @@ class Database():
             return None
 
         temp = cursor.fetchall()
-        year =  [table[0].split('_') for table in temp if contains(table[0],'_')]
+        print(temp)
+        year =  [table[0].split('_') for table in temp]
         
-        for i,j in year:
-            memo[j] += [i]
-        cursor.execute("desc may_2024")
-        print(cursor.fetchall()[:,1])
+        for i,j,k,l in year:
+            if i not in memo:
+                memo[i] = {}
+            if j not in memo[i]:
+                memo[i][j] = defaultdict(list)
+
+            memo[i][j][l] += memo[i][j][l] + [k]
+
         return memo
 
-    def getColumns(self,month,year):
-        cursor = self.db.cursor()
+    def getColumns(self,month,year,insti:str,type:str):
+        cursor = self.db.cursor(buffered=True)
 
         try:
-            cursor.execute(f'desc {month}_{year}')
+            cursor.execute(f'desc {insti}_{type}_{month}_{year}')
         except:
             print('MySQL Error Occured!')
             return None
@@ -86,30 +91,30 @@ class Database():
 
 
     # fetches data for That guy from table month_year
-    def fetchThat(self,month:str,year:int,emp_id:int) -> tuple[list[str]]:
-        cursor = self.db.cursor()
+    def fetchThat(self,month:str,year:int,emp_id:int,insti:str,type:str) -> tuple[list[str]]:
+        cursor = self.db.cursor(buffered=True)
 
         try:
-            cursor.execute(f" SELECT * FROM {month}_{year} where HR_EMP_CODE={emp_id}")
+            cursor.execute(f" SELECT * FROM {insti}_{type}_{month}_{year} where HR_EMP_CODE={emp_id}")
         except:
             print('MySQL Error Occured!')
             return None
         
-        columns = self.getColumns(month,year)
+        columns = self.getColumns(month,year,insti,type)
         return pd.DataFrame(cursor.fetchall(),columns=columns)
 
     # fetches all data from table month_year  
-    def fetchAll(self,month:str,year:int) -> dict[str,list[str]]:
-        cursor = self.db.cursor()
+    def fetchAll(self,month:str,year:int,insti:str,type:str) -> dict[str,list[str]]:
+        cursor = self.db.cursor(buffered=True)
 
         try:
-            cursor.execute(f"SELECT * FROM {month}_{year}")
+            cursor.execute(f"SELECT * FROM {insti}_{type}_{month}_{year}")
 
         except:
             print('Table does not exists')
             return None
         
-        columns = self.getColumns(month,year)
+        columns = self.getColumns(month,year,insti,type)
         return pd.DataFrame(cursor.fetchall(),columns=columns)
     
     # end database. RIP
@@ -124,7 +129,7 @@ def dataRefine(data):
     data.rename(columns={col:rename(col) for col in data.columns},inplace=True)
 
 # Must do these 3 steps
-pde = pd.read_excel('front/KJSIT_MAY_2023.xlsx')
+"""pde = pd.read_excel("front/KJSIT_MAY_2023.xlsx")
 dataRefine(pde)
 
 b = Database(
@@ -132,5 +137,4 @@ b = Database(
         user="root",
         password="1234",
         database="somaiya_salary"
-    )
-print(pde.columns)
+    )"""
